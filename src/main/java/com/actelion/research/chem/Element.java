435975a91dd12bab478f5932e2ad978b1276adc6
Bamboo <bamboo@chiron.actelion.com>
2017-08-11 04:01:24 +0200

    Sync of 2017_08_11-04_01_21

--- src/main/java/com/actelion/research/chem/Element.java
+++ src/main/java/com/actelion/research/chem/Element.java
@@ -33,6 +33,10 @@
 
 package com.actelion.research.chem;
 
+import com.actelion.research.util.Formatter;
+
+import java.util.Comparator;
+
 public class Element {
 
 	private int orderNumber;
@@ -47,12 +51,14 @@ public class Element {
 	
 	private double vdwRadius;
 
+	private double electronegativity;
+
 	public Element() {
 
 	}
 
 	public Element(int orderNumber, String name, String symbol,
-			double weight, double covalentRadius, double vdwRadius) {
+			double weight, double covalentRadius, double vdwRadius, double electronegativity) {
 
 		this.orderNumber = orderNumber;
 		
@@ -63,8 +69,10 @@ public class Element {
 		this.weight = weight;
 		
 		this.covalentRadius = covalentRadius;
-		
+
 		this.vdwRadius = vdwRadius;
+
+		this.electronegativity = electronegativity;
 	}
 
 	public int getOrderNumber() {
@@ -91,10 +99,64 @@ public class Element {
 		return vdwRadius;
 	}
 
+	public double getElectronegativity() {
+		return electronegativity;
+	}
+
+	public void setElectronegativity(double electronegativity) {
+		this.electronegativity = electronegativity;
+	}
+
 	public String toString() {
 		
 		return symbol;
 	}
-	
+
+
+	public String toStringForValueTable(){
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("new Element(");
+		sb.append(orderNumber);
+		sb.append(",");
+		sb.append("\"" + name + "\"");
+		sb.append(",");
+		sb.append("\"" + symbol + "\"");
+		sb.append(",");
+		sb.append(Formatter.format3(weight));
+		sb.append(",");
+		sb.append(Formatter.format3(covalentRadius));
+		sb.append(",");
+		sb.append(Formatter.format3(vdwRadius));
+		sb.append(",");
+		sb.append(Formatter.format3(electronegativity));
+		sb.append("),");
+
+		return sb.toString();
+
+	}
+
+
+	public static Comparator<Element> getComparatorOrderNumber(){
+
+		return new Comparator<Element>() {
+			@Override
+			public int compare(Element o1, Element o2) {
+
+				int cmp = 0;
+
+				if(o1.getOrderNumber() > o2.getOrderNumber()){
+					cmp=1;
+				}else if(o1.getOrderNumber() < o2.getOrderNumber()){
+					cmp=-1;
+				}
+
+				return cmp;
+			}
+		};
+	}
+
+
 
 }
