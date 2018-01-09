288a9661f3d4589114ac322052090c01c8cdae85
Bamboo <bamboo@chiron.actelion.com>
2018-01-09 04:01:26 +0100

    Sync of 2018_01_09-04_01_21

--- src/main/java/com/actelion/research/gui/hidpi/HiDPIIcon.java
+++ src/main/java/com/actelion/research/gui/hidpi/HiDPIIcon.java
@@ -16,34 +16,23 @@ public class HiDPIIcon extends ImageIcon {
 
 	public synchronized void paintIcon(Component c, Graphics g, int x, int y) {
 		if (HiDPIHelper.getRetinaScaleFactor() == 2) {
-			x = 1;
-			y = 1;
-			if (LookAndFeelHelper.isQuaQua()) {
-				x++;
+			Image image = getImage();
+			int width = image.getWidth(null) / 2;
+			int height = image.getHeight(null) / 2;
+
+			// because of double size image, x and y are too small and need to be corrected
+			// (in case of aqua x & y are 1 - size/2, in case of new substance x & y are 0)
+			x += width / 2;
+			y += height / 2;
+
+			if (LookAndFeelHelper.isNewSubstance()) {
+				// for some reason y needs to be adjusted by 1
 				y++;
 				}
-			else if (LookAndFeelHelper.isNewSubstance()) {
-				x--;
-				y--;
-				}
 
-			Image image = getImage();
-			int width = image.getWidth(null);
-			int height = image.getHeight(null);
-			g.drawImage(image, x, y, width / 2, height / 2, null);
+			g.drawImage(image, x, y, width, height, null);
 			}
 		else {
-			x = 1;
-			y = 1;
-			if (LookAndFeelHelper.isQuaQua()) {
-				x++;
-				y++;
-				}
-			else if (LookAndFeelHelper.isNewSubstance()) {
-				x--;
-				y--;
-				}
-
 			Image image = getImage();
 			g.drawImage(image, x, y, null);
 			}
