82e117f33604f14417a217a01265c881da99b218
Bamboo <bamboo@chiron.actelion.com>
2019-11-30 04:00:50 +0100

    Sync of 2019_11_30-04_00_47

--- src/main/java/com/actelion/research/chem/io/CompoundTableConstants.java
+++ src/main/java/com/actelion/research/chem/io/CompoundTableConstants.java
@@ -57,7 +57,7 @@ public interface CompoundTableConstants {
 
     String cReactionPartReaction = "reaction";  // this may only be used, if a molecule type descriptor is calculated from all merged reaction molecules
     String cReactionPartReactants = "reactants";
-    String cReactionPartCatalysts = "catalysts";
+    String cReactionPartPseudoCatalysts = "catalysts";
     String cReactionPartProducts = "products";
     String cReactionPartDelimiter = " of ";
 
