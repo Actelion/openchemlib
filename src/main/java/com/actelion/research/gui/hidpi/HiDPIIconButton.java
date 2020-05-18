737037ccdcdf69b8ed2751d36973ee62145c9d67
Thomas Sander <thomas.sander@idorsia.com>
2020-05-18 17:25:57 +0200

    now supports date and shows animated button for slider filter animation

--- src/main/java/com/actelion/research/gui/hidpi/HiDPIIconButton.java
+++ src/main/java/com/actelion/research/gui/hidpi/HiDPIIconButton.java
@@ -33,10 +33,9 @@
 
 package com.actelion.research.gui.hidpi;
 
-import com.actelion.research.gui.LookAndFeelHelper;
-
 import javax.swing.*;
 import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * Created by sandert on 04/12/15.
@@ -44,6 +43,8 @@ import java.awt.*;
 public class HiDPIIconButton extends JButton {
 	private String mImageName,mStyle;
 	private int mRotation;
+	private Icon[] mAnimationIcon;
+	private Animator mAnimator;
 
 	/**
 	 * Creates a button that, if image2 is given, toggles between two states indicated
@@ -63,7 +64,7 @@ public class HiDPIIconButton extends JButton {
 		 * Creates a button that, if image2 is given, toggles between two states indicated
 		 * by two different button images. The button optimizes its size to for QuaQua
 		 * and Substance look&feels and uses adequate higher resolution images on HiDPI monitors.
-		 * For Retina displays (Mac) it expects double resulution images named 'originalName@2x.png'.
+		 * For Retina displays (Mac) it expects double resolution images named 'originalName@2x.png'.
 		 *
 		 * @param imageName initial appearance
 		 * @param tooltip may be null
@@ -88,6 +89,33 @@ public class HiDPIIconButton extends JButton {
 			setToolTipText(tooltip);
 		}
 
+	/**
+	 * In order for a button to support an animation, it need additional images to follow after its normal appearance.
+	 * Animation image names are built from the button's imageName with appended '_0', '_1', etc.
+	 * This methods loads and prepares the additional images needed for the button animation.
+	 * @param animationImageCount number of animation images including the base image
+	 */
+	public void startAnimation(int animationImageCount) {
+		if (mAnimationIcon == null) {
+			mAnimationIcon = new Icon[animationImageCount];
+			mAnimationIcon[0] = getIcon();
+			for (int i=1; i<animationImageCount; i++) {
+				int index = mImageName.lastIndexOf('.');
+				String fileName = mImageName.substring(0, index)+"_"+i+mImageName.substring(index);
+				mAnimationIcon[i] = HiDPIHelper.createIcon(fileName, 0);
+				}
+			}
+
+		mAnimator = new Animator(animationImageCount);
+		mAnimator.start();
+		}
+
+	public void stopAnimation() {
+		if (mAnimator != null) {
+			mAnimator.stop();
+			}
+		}
+
 	private void updateIconSet() {
 		if (mImageName != null) {
 			setIcon(HiDPIHelper.createIcon(mImageName, mRotation));
@@ -104,5 +132,62 @@ public class HiDPIIconButton extends JButton {
 	public void updateUI() {
 		updateIconSet();
 		super.updateUI();
+		}
+
+	private class Animator implements Runnable {
+		private static final long FRAME_RATE = 100L;
+		private volatile long mStartMillis;
+		private volatile int mFrameCount;
+		private Thread mThread;
+
+		public Animator(int frameCount) {
+			mFrameCount = frameCount;
+		}
+
+		public void reset() {
+			mThread = null;
+		}
+
+		public void start() {
+			if (mThread == null) {
+				mThread = new Thread(this);
+				mThread.start();
+				mStartMillis = System.currentTimeMillis();
+			}
+		}
+
+		public void stop() {
+			mThread = null;
+		}
+
+		@Override
+		public void run() {
+			while (Thread.currentThread() == mThread) {
+				try {
+					SwingUtilities.invokeAndWait(new Runnable() {
+						int mRecentFrame = -1;
+
+						@Override
+						public void run() {
+							if (mThread != null) {
+								long totalDelay = System.currentTimeMillis() - mStartMillis;
+								int currentFrame = (int)(totalDelay / FRAME_RATE);
+								if (mRecentFrame < currentFrame) {
+									mRecentFrame = currentFrame;
+									setIcon(mAnimationIcon[currentFrame % mFrameCount]);
+									}
+								}
+							}
+						});
+					}
+				catch (InvocationTargetException ite) {}
+				catch (InterruptedException ie) {}
+
+				try {
+					Thread.sleep(FRAME_RATE - (System.currentTimeMillis() - mStartMillis) % FRAME_RATE);
+					}
+				catch (InterruptedException ie) {}
+				}
+			}
 		}
 	}
