afdcf9a7a138a1f763fc56f8e7a51c133e561cc0
Thomas Sander <thomas.sander@idorsia.com>
2020-09-13 19:28:44 +0200

    added creation of column lists and their usage to quickly show/hide column subsets

--- src/main/java/com/actelion/research/gui/JChemistryView.java
+++ src/main/java/com/actelion/research/gui/JChemistryView.java
@@ -1,12 +1,34 @@
 /*
- * @(#)JExtendedStructureView.java	<Version> <Date>
+ * Copyright (c) 1997 - 2016
+ * Actelion Pharmaceuticals Ltd.
+ * Gewerbestrasse 16
+ * CH-4123 Allschwil, Switzerland
  *
- * Copyright 2004 Actelion Ltd. All Rights Reserved.
+ * All rights reserved.
  *
- * This software is the proprietary information of Actelion Ltd.
- * Use is subject to license terms.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * @author      Thomas Sander
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the the copyright holder nor the
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  */
 
 package com.actelion.research.gui;
@@ -15,7 +37,10 @@ import com.actelion.research.chem.*;
 import com.actelion.research.chem.io.RXNFileParser;
 import com.actelion.research.chem.reaction.Reaction;
 import com.actelion.research.gui.clipboard.ClipboardHandler;
-import com.actelion.research.gui.dnd.*;
+import com.actelion.research.gui.dnd.MoleculeDropAdapter;
+import com.actelion.research.gui.dnd.MoleculeTransferable;
+import com.actelion.research.gui.dnd.ReactionDropAdapter;
+import com.actelion.research.gui.dnd.ReactionTransferable;
 import com.actelion.research.util.ColorHelper;
 import com.actelion.research.util.CursorHelper;
 
@@ -29,7 +54,7 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class JChemistryView extends JComponent
-				 implements ActionListener,DragGestureListener,DragSourceListener,MouseListener,MouseMotionListener {
+				 implements ActionListener, DragGestureListener, DragSourceListener, MouseListener, MouseMotionListener {
 	public static final int PASTE_AND_DROP_OPTION_ALLOW_FRAGMENT_STATE_CHANGE = 1;
 	public static final int PASTE_AND_DROP_OPTION_KEEP_ATOM_COLORS = 2;
 	public static final int PASTE_AND_DROP_OPTION_KEEP_BOND_HIGHLIGHTING = 4;
@@ -54,9 +79,9 @@ public class JChemistryView extends JComponent
 	private static final int ALLOWED_DRAG_ACTIONS = DnDConstants.ACTION_COPY_OR_MOVE;
 	private static final int ALLOWED_DROP_ACTIONS = DnDConstants.ACTION_COPY_OR_MOVE;
 
-	private ExtendedDepictor	mDepictor;
+	private ExtendedDepictor mDepictor;
 	private ArrayList<StructureListener> mListener;
-	private Dimension			mSize;
+	private Dimension mSize;
 	private int					mChemistryType,mUpdateMode,mDisplayMode,mDragMolecule,mCopyOrDragActions,mPasteOrDropActions,mPasteAndDropOptions;
 	private boolean				mShowBorder,mIsDraggingThis,mAllowDropOrPasteWhenDisabled;
 	private Color				mFragmentNoColor;
