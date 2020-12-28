63b1ae63c3b28501e15e3b3c83ca9d326f5dc63a
korffmo1 <modest.korff@actelion.com>
2020-12-28 08:53:48 +0100

    added interface for Flexophore descriptors

--- src/main/java/com/actelion/research/chem/descriptor/flexophore/IDescriptorHandlerFlexophore.java
+++ src/main/java/com/actelion/research/chem/descriptor/flexophore/IDescriptorHandlerFlexophore.java
@@ -1,10 +1,13 @@
 package com.actelion.research.chem.descriptor.flexophore;
 
+import com.actelion.research.chem.StereoMolecule;
 import com.actelion.research.chem.descriptor.DescriptorHandler;
 import com.actelion.research.util.graph.complete.IObjectiveCompleteGraph;
 
 public interface IDescriptorHandlerFlexophore extends DescriptorHandler {
 
     IObjectiveCompleteGraph getObjectiveCompleteGraph();
+
+    MolDistHistViz createVisualDescriptor(StereoMolecule mol);
 
 }
