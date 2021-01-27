50fb23556cbe7ac92f82b69a42ea3f750af7be0d
korffmo1 <modest.korff@actelion.com>
2021-01-27 12:50:34 +0100

    fixed border issues for sliding window filter-->changes very narrow distances histograms in Flexophore

--- src/main/java/com/actelion/research/calc/filter/SlidingWindow.java
+++ src/main/java/com/actelion/research/calc/filter/SlidingWindow.java
@@ -11,107 +11,84 @@ import com.actelion.research.util.ArrayUtils;
  */
 public class SlidingWindow {
 
-
     private double [] arrFilter;
 
-    private int lenFilHalf;
+    private int l;
+    private int l_half;
 
     public SlidingWindow(double[] arrFilter) {
-
         this.arrFilter = arrFilter;
-
         if(arrFilter.length % 2 == 0){
-
             throw new RuntimeException("Odd number of filter values needed.");
-
         }
 
-        lenFilHalf = arrFilter.length / 2;
-
+        l = arrFilter.length;
+        l_half = arrFilter.length/2;
     }
 
     public double [] filter(double [] a) {
 
-        double[] aa = new double[a.length];
-
-        System.arraycopy(a, 0, aa, 0, aa.length);
-
-        int end = a.length - lenFilHalf;
-
-        for (int i = lenFilHalf; i < end; i++) {
-
+        double [] aa = new double[a.length+l*2];
+        double [] aaa = new double[a.length+l*2];
+        System.arraycopy(a, 0, aa, l, a.length);
+        int end = aa.length- l_half;
+        for (int i = l_half; i < end; i++) {
             double v = 0;
-
             for (int j = 0; j < arrFilter.length; j++) {
-
-                int ind = i - lenFilHalf + j;
-
-                v += a[ind] * arrFilter[j];
-
+                int ind = i - l_half + j;
+                v += aa[ind] * arrFilter[j] + 0.5;
             }
-
-            aa[i] = v;
+            aaa[i] = v;
         }
 
+        aa = new double[a.length];
+        System.arraycopy(aaa, l, aa, 0, aa.length);
         return aa;
+
     }
 
     public int [] filter(int [] a) {
 
-        int[] aa = new int[a.length];
-
-        System.arraycopy(a, 0, aa, 0, aa.length);
-
-        int end = a.length - lenFilHalf;
-
-        for (int i = lenFilHalf; i < end; i++) {
-
-
+        int [] aa = new int[a.length+l*2];
+        int [] aaa = new int[a.length+l*2];
+        System.arraycopy(a, 0, aa, l, a.length);
+        int end = aa.length- l_half;
+        for (int i = l_half; i < end; i++) {
             int v = 0;
-
             for (int j = 0; j < arrFilter.length; j++) {
-
-                int ind = i - lenFilHalf + j;
-
-                v += (int)(a[ind] * arrFilter[j] + 0.5);
-
+                int ind = i - l_half + j;
+                v += (int)(aa[ind] * arrFilter[j] + 0.5);
             }
-
-            aa[i] = v;
+            aaa[i] = v;
         }
 
+        aa = new int[a.length];
+        System.arraycopy(aaa, l, aa, 0, aa.length);
         return aa;
     }
 
     public byte [] filter(byte [] a) {
 
-        byte [] aa = new byte[a.length];
+        byte [] aa = new byte[a.length+l*2];
+        byte [] aaa = new byte[a.length+l*2];
+        System.arraycopy(a, 0, aa, l, a.length);
+        int end = aa.length- l_half;
 
-        System.arraycopy(a, 0, aa, 0, aa.length);
-
-        int end = a.length - lenFilHalf;
-
-        for (int i = lenFilHalf; i < end; i++) {
-
-
+        for (int i = l_half; i < end; i++) {
             byte v = 0;
-
             for (int j = 0; j < arrFilter.length; j++) {
-
-                int ind = i - lenFilHalf + j;
-
-                v += (byte)(a[ind] * arrFilter[j] + 0.5);
-
+                int ind = i - l_half + j;
+                v += (byte)(aa[ind] * arrFilter[j] + 0.5);
             }
-
-            aa[i] = v;
+            aaa[i] = v;
         }
+        aa = new byte[a.length];
+        System.arraycopy(aaa, l, aa, 0, aa.length);
 
         return aa;
     }
 
     public static void main(String[] args) {
-
 
         int [] a = {0,0,0,0,100,100,0,0,0,0,0};
 
@@ -123,9 +100,5 @@ public class SlidingWindow {
 
         System.out.println(ArrayUtils.toString(aa));
     }
-
-
-
-
 
 }
