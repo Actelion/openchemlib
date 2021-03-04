2cc4a775cc1af9d0312270d9b8b75ce2d06fa27f
korffmo1 <modest.korff@actelion.com>
2021-03-04 10:40:59 +0100

    added enum for classification attributes

---
+++ src/main/java/com/actelion/research/calc/classification/ClassificationAttr.java
@@ -0,0 +1,14 @@
+package com.actelion.research.calc.classification;
+
+/**
+ * Modest v. Korff
+ * Idorsia Pharmaceuticals Ltd.
+ * 04.03.2021 Start implementation
+ **/
+public enum ClassificationAttr {
+    POSITIVE, NEGATIVE, NONE;
+    public static final String TAG_TEST_CLASS = "TestClassPosNeg";
+    public static final String POS = "positive";
+    public static final String NEG = "negative";
+    public static final String NO = "none";
+}
