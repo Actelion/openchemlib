559b9649b82af1a6c480c4b7c57687249ec50d23
Thomas Sander <thomas.sander@idorsia.com>
2021-06-14 21:24:36 +0200

    fixed small issue with valence calculation in case of explicit delocalized bonds

--- src/main/java/com/actelion/research/chem/ExtendedMolecule.java
+++ src/main/java/com/actelion/research/chem/ExtendedMolecule.java
@@ -525,18 +525,26 @@ public class ExtendedMolecule extends Molecule implements Serializable {
 	public int getOccupiedValence(int atom) {
 		ensureHelperArrays(cHelperNeighbours);
 
+		boolean piElectronsFound = false;
+		boolean delocalizedBondFound = false;
 		int valence = 0;
-		int delocalizedCount = 0;
 		for (int i=0; i<mAllConnAtoms[atom]; i++) {
-			if (!mIsFragment || (mAtomQueryFeatures[mConnAtom[atom][i]] & cAtomQFExcludeGroup) == 0)
-				valence += mConnBondOrder[atom][i];
-			int bond = mConnBond[atom][i];
-			if (mBondType[bond] == cBondTypeDelocalized
-			 || (mIsFragment && (mBondQueryFeatures[bond] & cBondQFBondTypes) == cBondQFDelocalized))
-				delocalizedCount++;
+			if (!mIsFragment || (mAtomQueryFeatures[mConnAtom[atom][i]] & cAtomQFExcludeGroup) == 0) {
+				int order = mConnBondOrder[atom][i];
+				valence += order;
+				if (order > 1)
+					piElectronsFound = true;
+
+				int bond = mConnBond[atom][i];
+				if (mBondType[bond] == cBondTypeDelocalized)
+					delocalizedBondFound = true;
+				}
 			}
 
-		return valence + (delocalizedCount >= 2 ? 1 : 0);
+		if (delocalizedBondFound && !piElectronsFound)
+			valence++;
+
+		return valence;
 		}
 
 
@@ -1528,7 +1536,8 @@ public class ExtendedMolecule extends Molecule implements Serializable {
 	 * @return
 	 */
 	public boolean isDelocalizedBond(int bond) {
-		return (mBondFlags[bond] & cBondFlagDelocalized) != 0;
+		return (mBondFlags[bond] & cBondFlagDelocalized) != 0
+			 || (mIsFragment && (mBondQueryFeatures[bond] & cBondQFBondTypes) == cBondQFDelocalized);
 		}
 
 
