ebbe1163c5f9035fb5fea7db5f16d071680b13f8
Thomas Sander <thomas.sander@idorsia.com>
2022-01-21 15:58:34 +0100

    now shows Flexophore atom contributions in multiple colors

--- src/main/java/com/actelion/research/chem/descriptor/flexophore/FlexophoreAtomContributionColors.java
+++ src/main/java/com/actelion/research/chem/descriptor/flexophore/FlexophoreAtomContributionColors.java
@@ -1,73 +1,135 @@
 package com.actelion.research.chem.descriptor.flexophore;
 
-import com.actelion.research.chem.Molecule;
-import com.actelion.research.chem.StereoMolecule;
 import com.actelion.research.chem.descriptor.DescriptorHandlerFlexophore;
 import com.actelion.research.chem.descriptor.flexophore.completegraphmatcher.ObjectiveBlurFlexophoreHardMatchUncovered;
 
-public class FlexophoreAtomContributionColors {
-	private int[] mARGB;
-	private float[] mRadius;
+import java.awt.*;
 
-	public FlexophoreAtomContributionColors(StereoMolecule mol, MolDistHist molFlexophore, MolDistHist refFlexophore) {
-		if (mol != null && molFlexophore != null && refFlexophore != null) {
-			mol.ensureHelperArrays(Molecule.cHelperRings);
-			float[][] atomContribution = getFlexophoreAtomContributions(molFlexophore, refFlexophore, mol.getAllAtoms());
-			if (atomContribution != null) {
-				mARGB = new int[mol.getAtoms()];
-				mRadius = new float[mol.getAtoms()];
-				for (int atom=0; atom<mol.getAtoms(); atom++) {
-					if (atomContribution[0][atom] > 0f) {
-						final float f = 4f;
-						// we stretch the range, because original values always tend to be larger than 0.75
-						float nodeSimilarity = Math.max(0f, Math.min(1f, Math.round(atomContribution[0][atom] * f - f + 1f)));
-//						int color = Color.HSBtoRGB(nodeSimilarity, 1f, 1f);
-						int alpha = Math.round(nodeSimilarity * 255);
-						mARGB[atom] = (alpha << 24) | 255;
-						mRadius[atom] = atomContribution[1][atom];
+public class FlexophoreAtomContributionColors {
+	private static final int[] cDiverseColor = {
+			0x004900FF, 0x00FF002A, 0x0000AD66, 0x00FFFF00, 0x004FD1F8, 0x00DDA0F6, 0x00FFAB19, 0x00C0FFD0,
+			0x00FF00D8, 0x00AA6B58, 0x00BCBCBC, 0x005E6B26, 0x006CFF00, 0x00FFBC97, 0x00B4C732, 0x00A60000 } ;
+
+	private MolDistHist mMolFlexophore,mRefFlexophore;
+	private int[] mRefARGB,mMolARGB;
+	private float[] mRefRadius,mMolRadius;
+
+	public FlexophoreAtomContributionColors(MolDistHist molFlexophore, MolDistHist refFlexophore, int molAtomCount, int refAtomCount) {
+		// we stretch the range, because original values always tend to be larger than 0.75
+		final float NODE_SIMILARITY_FACTOR = 4f;
+
+		mMolFlexophore = molFlexophore;
+		mRefFlexophore = refFlexophore;
+
+		if (molAtomCount != 0 && refAtomCount != 0 && molFlexophore != null && refFlexophore != null) {
+			int[][] molNodeAtom = molFlexophore.getNodeAtoms();
+			int[][] refNodeAtom = refFlexophore.getNodeAtoms();
+
+			if (molNodeAtom != null && refNodeAtom != null) {
+				DescriptorHandlerFlexophore dhFlexophore = new DescriptorHandlerFlexophore();
+				ObjectiveBlurFlexophoreHardMatchUncovered objectiveBlurFlexophoreHardMatchUncovered = dhFlexophore.getObjectiveCompleteGraph();
+
+				ModelSolutionSimilarity modelSolutionSimilarity = dhFlexophore.getBestMatch(molFlexophore, refFlexophore);
+				if (modelSolutionSimilarity != null) {
+					mMolARGB = new int[molAtomCount];
+					mRefARGB = new int[refAtomCount];
+					mMolRadius = new float[molAtomCount];
+					mRefRadius = new float[refAtomCount];
+
+					int matchingNodeCount = modelSolutionSimilarity.getSizeHeap();
+
+					objectiveBlurFlexophoreHardMatchUncovered.setBase(molFlexophore);
+					objectiveBlurFlexophoreHardMatchUncovered.setQuery(refFlexophore);
+
+					int[] colorList = createDiverseColorList(matchingNodeCount);
+
+					for (int i=0; i<matchingNodeCount; i++) {
+						int molNode = modelSolutionSimilarity.getIndexBaseFromHeap(i);
+						int refNode = modelSolutionSimilarity.getIndexQueryFromHeap(i);
+
+						float nodeSimilarity = modelSolutionSimilarity.getSimilarityNode(i);
+						float edgeSimilarity = objectiveBlurFlexophoreHardMatchUncovered.getSimilarityHistogramsForNode(modelSolutionSimilarity, i);
+
+						if (nodeSimilarity > 0f) {
+							nodeSimilarity = Math.max(0f, Math.min(1f, Math.round(nodeSimilarity * NODE_SIMILARITY_FACTOR - NODE_SIMILARITY_FACTOR + 1f)));
+
+							for (int atom:molNodeAtom[molNode]) {
+								int alpha = Math.round(nodeSimilarity * 255);
+								mMolARGB[atom] = (alpha << 24) | colorList[i];
+								mMolRadius[atom] = edgeSimilarity;
+							}
+
+							for (int atom:refNodeAtom[refNode]) {
+								int alpha = Math.round(nodeSimilarity * 255);
+								mRefARGB[atom] = (alpha << 24) | colorList[i];
+								mRefRadius[atom] = edgeSimilarity;
+							}
 						}
+/*
+						float molNodeSimilarity = modelSolutionSimilarity.getSimilarityNode(refNode);
+						float refNodeSimilarity = modelSolutionSimilarity.getSimilarityNode(molNode);
+
+						if (molNodeSimilarity > 0f) {
+							molNodeSimilarity = Math.max(0f, Math.min(1f, Math.round(molNodeSimilarity * NODE_SIMILARITY_FACTOR - NODE_SIMILARITY_FACTOR + 1f)));
+							for (int atom:molNodeAtom[molNode]) {
+								int alpha = Math.round(molNodeSimilarity * 255);
+								mMolARGB[atom] = (alpha << 24) | colorList[i];
+								mMolRadius[atom] = edgeSimilarity;
+							}
+						}
+
+						if (refNodeSimilarity > 0f) {
+							refNodeSimilarity = Math.max(0f, Math.min(1f, Math.round(refNodeSimilarity * NODE_SIMILARITY_FACTOR - NODE_SIMILARITY_FACTOR + 1f)));
+							for (int atom:refNodeAtom[refNode]) {
+								int alpha = Math.round(refNodeSimilarity * 255);
+								mRefARGB[atom] = (alpha << 24) | colorList[i];
+								mRefRadius[atom] = edgeSimilarity;
+							}
+						}
+ */
 					}
 				}
 			}
 		}
-
-	public int[] getARGB() {
-		return mARGB;
-		}
-
-	public float[] getRadius() {
-		return mRadius;
-		}
-
-	private float[][] getFlexophoreAtomContributions(MolDistHist mdhMol, MolDistHist mdhRef, int molAtomCount) {
-		int[][] molNodeAtom = mdhMol.getNodeAtoms();
-		if (molNodeAtom == null)
-			return null;
-
-		DescriptorHandlerFlexophore dhFlexophore = new DescriptorHandlerFlexophore();
-		ObjectiveBlurFlexophoreHardMatchUncovered objectiveBlurFlexophoreHardMatchUncovered = dhFlexophore.getObjectiveCompleteGraph();
-
-		ModelSolutionSimilarity modelSolutionSimilarity = dhFlexophore.getBestMatch(mdhMol, mdhRef);
-		if (modelSolutionSimilarity == null)
-			return null;
-
-		int heap = modelSolutionSimilarity.getSizeHeap();
-
-		objectiveBlurFlexophoreHardMatchUncovered.setBase(mdhMol);
-		objectiveBlurFlexophoreHardMatchUncovered.setQuery(mdhRef);
-
-		float[][] atomContribution = new float[2][molAtomCount];
-
-		for (int i=0; i<heap; i++) {
-			int molNode = modelSolutionSimilarity.getIndexBaseFromHeap(i);
-			int refNode = modelSolutionSimilarity.getIndexQueryFromHeap(i);
-
-			for (int atom:molNodeAtom[molNode]) {
-				atomContribution[0][atom] = modelSolutionSimilarity.getSimilarityNode(refNode);
-				atomContribution[1][atom] = objectiveBlurFlexophoreHardMatchUncovered.getSimilarityHistogramsForNode(modelSolutionSimilarity, i);
-				}
-			}
-
-		return atomContribution;
-		}
 	}
+
+	public MolDistHist getMolFlexophore() {
+		return mMolFlexophore;
+	}
+
+	public MolDistHist getRefFlexophore() {
+		return mRefFlexophore;
+	}
+
+	public int[] getMolARGB() {
+		return mMolARGB;
+	}
+
+	public int[] getRefARGB() {
+		return mRefARGB;
+	}
+
+	public float[] getMolRadius() {
+		return mMolRadius;
+	}
+
+	public float[] getRefRadius() {
+		return mRefRadius;
+	}
+
+	private int[] createDiverseColorList(int colorCount) {
+		if (colorCount <= cDiverseColor.length)
+			return cDiverseColor;
+
+		int divisor = ((colorCount & 1) == 0) ? colorCount : colorCount+1;
+		final float[] s = { 1.0f, 0.4f, 0.8f, 1.0f, 1.0f, 0.6f, 0.8f, 1.0f };
+		final float[] b = { 0.8f, 1.0f, 1.0f, 0.6f, 0.8f, 1.0f, 1.0f, 0.4f };
+		int[] colorList = new int[colorCount];
+		for (int i=0; i<colorCount; i++) {
+			float hue = (float)i / (float)divisor;
+			colorList[i] = Color.HSBtoRGB(hue, s[i & 7], b[i & 7]);
+		}
+
+		return colorList;
+	}
+}
