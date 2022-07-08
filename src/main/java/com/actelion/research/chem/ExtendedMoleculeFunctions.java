a783bff17c63312aa2e30d92efc4b1d585fe3c38
korffmo <modest.korff@idorsia.com>
2022-07-08 17:29:07 +0200

    Update RingHelper, largest ring finder working. Replaced old eclosed graph check in SubGraphExtractor.

--- src/main/java/com/actelion/research/chem/ExtendedMoleculeFunctions.java
+++ src/main/java/com/actelion/research/chem/ExtendedMoleculeFunctions.java
@@ -91,9 +91,6 @@ public class ExtendedMoleculeFunctions {
 	public static final String [] arrRGroupsSymbol = {"R1","R2","R3","R4","R5","R6","R7","R8","R9","R10","R11","R12","R13","R14","R15","R16"};
 
 
-
-
-
 	public static void makeSkeleton(StereoMolecule mol) {
 		for (int bond=0; bond<mol.getAllBonds(); bond++)
 			mol.setBondType(bond, Molecule.cBondTypeSingle);
@@ -760,9 +757,7 @@ public class ExtendedMoleculeFunctions {
 	}
 
 	public final static int [][] getTopologicalDistanceMatrix(StereoMolecule mol) {
-
 		return getNumberOfBondsBetweenAtoms(mol, mol.getBonds(), null);
-
 	}
 
 	/**
