e7f26890be047baca18009eec1685b952dc20436
thomas <thomas@openmolecules.org>
2025-09-22 09:59:17 +0200

    bugfix

--- src/main/java/com/actelion/research/util/FastSpline.java
+++ src/main/java/com/actelion/research/util/FastSpline.java
@@ -40,27 +40,27 @@ import java.util.*;
 public final class FastSpline {
    
 	public final static class Polynome  {
-		private final double coeffs[]; //x0 x1 x2 x3 
+		private final double[] coeffs; //x0 x1 x2 x3
 		
 		public Polynome(double[] coeffs) {
 			this.coeffs = coeffs;			
 		}
 		
-		public final Polynome derivative() {
+		public Polynome derivative() {
 			return new Polynome(new double[] {coeffs[1], 2*coeffs[2], 3*coeffs[3], 0});
 		}
 
-		public final double value(double x) {
+		public double value(double x) {
 			return coeffs[0]+x*(coeffs[1]+x*(coeffs[2]+x*coeffs[3]));			
 		}
-		public final double[] getCoefficients() {
+		public double[] getCoefficients() {
 			return coeffs;
 		}
 	}
-    
-	
+
+
     /** Spline segment interval delimiters (knots).   Size is n+1 for n segments. */
-    private final double knots[];
+    private final double[] knots;
 
     /**
      * The polynomial functions that make up the spline.  The first element
@@ -69,20 +69,19 @@ public final class FastSpline {
      * evaluating these functions at <code>(x - knot[i])</code> where i is the
      * knot segment to which x belongs.
      */
-    private final Polynome polynomials[];
+    private final Polynome[] polynomials;
     
     /** 
      * Number of spline segments = number of polynomials
      *  = number of partition points - 1 
      */
     private final int n;
-    
 
     /**
      * Construct a polynomial spline function with the given segment delimiters
      * and interpolating polynomials.
      */
-    public FastSpline(double knots[], Polynome polynomials[]) {
+    public FastSpline(double[] knots, Polynome[] polynomials) {
         this.n = knots.length -1;
         this.knots = new double[n + 1];
         this.polynomials = new Polynome[n];
@@ -94,20 +93,21 @@ public final class FastSpline {
     /**
      * Compute the value for the function.
      */
-    public final double value(double v) throws ArrayIndexOutOfBoundsException {    	
+    public double value(double v) throws ArrayIndexOutOfBoundsException {
         int i = Arrays.binarySearch(knots, v);
         if (i < 0) i = -i - 2;
-        if (i < 0) i = 0; 
+        if (i < 0) i = 0;
+		if ( i >= polynomials.length)
+			i--;
         return polynomials[i].value(v - knots[i]);
     }
         
     /**
      * Returns the derivative of the polynomial spline function as a PolynomialSplineFunction
      */
-    public final FastSpline derivative() {
-    	Polynome derivativePolynomials[] = new Polynome[n];
+    public FastSpline derivative() {
+    	Polynome[] derivativePolynomials = new Polynome[n];
         for (int i = 0; i < n; i++) derivativePolynomials[i] = polynomials[i].derivative();
         return new FastSpline(knots, derivativePolynomials);
     }
-
 }
